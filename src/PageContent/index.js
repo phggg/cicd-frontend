3648a9151cd351d06ba4dddb3bb6df9fc095bf10
panghao <panghao@tianyancha.com>
2022-07-18 16:25:53 +0800

    feat: test deploy2

--- src/PageContent/index.js
+++ src/PageContent/index.js
@@ -70,7 +70,7 @@ const PageContent = () => {
         icon={<PlusOutlined/>}
         onClick={handleClick}
       >
-        新增用户(新版本3)
+        新增用户(新版本4)
       </Button>
       <DataTable
         style={{marginTop: '10px'}}
